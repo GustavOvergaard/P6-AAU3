150656538197b7e7cdadf81acfda5122f3d165ec
Jako7115 <124875252+Jako7115@users.noreply.github.com>
2023-04-21 08:56:00 +0200

    Add files via upload

---
+++ noise_test/IMU_noise.m
@@ -0,0 +1,16 @@
+clc
+clear all
+close all
+load('noiseData.mat');
+n=10755;
+
+for i = 1:10755
+    tid(i) = 0.01 * i;
+end
+plot(tid,data-mean(data))
+
+temp=0;
+for i = 1:10755
+    temp = temp + (data(i)-0)^2;
+end
+var=1/(n-1)*temp

37fc8e44339a53abe75390833ec675a402849ffd
Jako7115 <124875252+Jako7115@users.noreply.github.com>
2023-05-01 11:40:36 +0200

    Add files via upload

---
+++ recoverytest/outer/udsvingsdataouter.m
@@ -0,0 +1,14 @@
+clc
+clear all
+close all
+
+load('data1.mat');
+load('data2.mat');
+load('data3.mat');
+load('data4.mat');
+load('data6.mat');
+load('data7.mat');
+realdata=[data1(50:2900,1);data2(570:4550,1);data6(800:2050,1);data7(200:1000,1)]
+
+plot(realdata);
+
